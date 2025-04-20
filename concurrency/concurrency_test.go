f66b295021a8bbf0681a86d97b5e5a12ec24aaa9
Ajani Green <ajani.green@outlook.com>
2025-04-20 12:42:47 -0500

    Finish concurrency learning

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -3,18 +3,21 @@ package concurrency
 import (
 	"fmt"
 	"slices"
+	"sync"
 	"testing"
 	"time"
 )
 
 func Test(t *testing.T) {
-	test1(t)
-	test2(t)
-	test3(t)
-	test4(t)
-	test5(t)
-	test6(t)
-	test_process_messages_challenge(t)
+	// test1(t)
+	// test2(t)
+	// test3(t)
+	// test4(t)
+	// test5(t)
+	// test6(t)
+	// test_process_messages_challenge(t)
+
+	test7(t)
 }
 
 func test1(t *testing.T) {
@@ -549,6 +552,78 @@ Test Passed:
   expected: %v
   actual:   %v
 `, test.messages, test.expect, result)
+		}
+	}
+
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+func test7(t *testing.T) {
+	type testCase struct {
+		email string
+		count int
+	}
+
+	runCases := []testCase{
+		{"norman@bates.com", 23},
+		{"marion@bates.com", 67},
+	}
+
+	submitCases := append(runCases, []testCase{
+		{"lila@bates.com", 31},
+		{"sam@bates.com", 453},
+	}...)
+
+	testCases := runCases
+	if withSubmit {
+		testCases = submitCases
+	}
+
+	skipped := len(submitCases) - len(testCases)
+
+	passCount := 0
+	failCount := 0
+
+	for _, test := range testCases {
+		sc := safeCounter{
+			counts: make(map[string]int),
+			mu:     &sync.RWMutex{},
+		}
+		var wg sync.WaitGroup
+		for i := 0; i < test.count; i++ {
+			wg.Add(1)
+			go func(email string) {
+				sc.inc(email)
+				wg.Done()
+			}(test.email)
+		}
+		wg.Wait()
+
+		sc.mu.RLock()
+		defer sc.mu.RUnlock()
+		if output := sc.val(test.email); output != test.count {
+			failCount++
+			t.Errorf(`---------------------------------
+Test Failed:
+  email: %v
+  count: %v
+  expected count: %v
+  actual count:   %v
+`, test.email, test.count, test.count, output)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+Test Passed:
+  email: %v
+  count: %v
+  expected count: %v
+  actual count:   %v
+`, test.email, test.count, test.count, output)
 		}
 	}
 
